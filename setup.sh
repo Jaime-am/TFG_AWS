86ba195912b2441e94663f9592beb91638a0ee60
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-06-10 13:11:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo rm -r /var/www/html/*
-sudo mv -r /TFG_AWS/* /var/www/html
-sudo mv -r /var/www/html/sign-up-login-form/dist/* /var/www/html
+sudo mv /TFG_AWS/* /var/www/html
+sudo mv /var/www/html/sign-up-login-form/dist/* /var/www/html
