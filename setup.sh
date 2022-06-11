313b6081f57c0d7e1c7602d11628f1a9a710b69f
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-06-11 14:11:20 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt install mysql-server -y
-sudo apt install mariadb-server -y
+##sudo apt install mariadb-server -y
 sudo service mysql start
-sudo service mariadb start
+##sudo service mariadb start
 sudo rm -r /var/www/html/*
 sudo mv /TFG_AWS/* /var/www/html
 sudo mv /var/www/html/sign-up-login-form/dist/* /var/www/html
