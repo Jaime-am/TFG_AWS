96be9eb16fa0b4e717c4367327d444f6c0636e25
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-06-11 20:41:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-sudo apt install apache2 -y
-sudo apt install mysql-server -y
-sudo apt install php -y
-sudo apt install php-mysqli -y
-sudo service apache2 start
-sudo service mysql start
-sudo mysql -u root <<-EOF
+apt install apache2 -y
+apt install mysql-server -y
+apt install php -y
+apt install php-mysqli -y
+service apache2 start
+service mysql start
+mysql -u root <<-EOF
 CREATE USER 'jaime'@'localhost' IDENTIFIED BY ''; #crea usuario en mysql
 GRANT ALL PRIVILEGES ON * . * TO 'jaime'@'localhost'; #da maximos privilegios al usuario creado
 flush privileges; #reinicia todos los permisos para aplicar cambios
 EOF
-sudo mysql -u jaime <<-EOF
+mysql -u jaime <<-EOF
 create database web; #crea DB para el login web
 use web;
 CREATE TABLE usuarios (nombre VARCHAR(30),apellidos VARCHAR(30),user VARCHAR(30) PRIMARY KEY UNIQUE,password VARCHAR(100));
 INSERT INTO usuarios (nombre, apellidos, user, password) VALUES ('test', 'test', 'test@test.es', 'test');
 EOF
 
-sudo rm -r /var/www/html/*
-sudo mv /TFG_AWS/* /var/www/html
-sudo mv /var/www/html/sign-up-login-form/dist/* /var/www/html
+rm -r /var/www/html/*
+mv /TFG_AWS/* /var/www/html
+mv /var/www/html/sign-up-login-form/dist/* /var/www/html
