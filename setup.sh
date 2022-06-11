d37860a6b1f503111f49ef6c5bbc5fb7cf48ac77
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-06-11 22:12:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ sudo apt install php -y
 sudo apt install php-mysqli -y
 sudo service apache2 start
 sudo service mysql start
-mysql -u root <<-EOF
+sudo mysql -u root <<-EOF
 CREATE USER 'jaime'@'localhost' IDENTIFIED BY ''; #crea usuario en mysql
 GRANT ALL PRIVILEGES ON * . * TO 'jaime'@'localhost'; #da maximos privilegios al usuario creado
 flush privileges; #reinicia todos los permisos para aplicar cambios
 EOF
-mysql -u jaime <<-EOF
+sudo mysql -u jaime <<-EOF
 create database web; #crea DB para el login web
 use web;
 CREATE TABLE usuarios (nombre VARCHAR(30),apellidos VARCHAR(30),user VARCHAR(30) PRIMARY KEY UNIQUE,password VARCHAR(100));
