a12d433cf1782b6626fb089f5202b05383d73893
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-06-11 22:11:21 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ sudo apt install apache2 -y
 sudo apt install mysql-server -y
 sudo apt install php -y
 sudo apt install php-mysqli -y
-service apache2 start
-service mysql start
+sudo service apache2 start
+sudo service mysql start
 mysql -u root <<-EOF
 CREATE USER 'jaime'@'localhost' IDENTIFIED BY ''; #crea usuario en mysql
 GRANT ALL PRIVILEGES ON * . * TO 'jaime'@'localhost'; #da maximos privilegios al usuario creado
@@ -18,6 +18,6 @@ CREATE TABLE usuarios (nombre VARCHAR(30),apellidos VARCHAR(30),user VARCHAR(30)
 INSERT INTO usuarios (nombre, apellidos, user, password) VALUES ('test', 'test', 'test@test.es', 'test');
 EOF
 
-rm -r /var/www/html/*
-mv /TFG_AWS/* /var/www/html
-mv /var/www/html/sign-up-login-form/dist/* /var/www/html
+sudo rm -r /var/www/html/*
+sudo mv /TFG_AWS/* /var/www/html
+sudo mv /var/www/html/sign-up-login-form/dist/* /var/www/html
