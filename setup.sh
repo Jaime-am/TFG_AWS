ec1554f2541aa17204af00c9b1cdea5926d66fe2
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-06-11 17:03:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,15 @@
 
 sudo apt install mysql-server -y
 sudo service mysql start
-sudo mysql -u root -e "CREATE USER 'jaime'@'localhost' IDENTIFIED BY '';" //crea usuario en mysql
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'jaime'@'localhost';" //da maximos privilegios al usuario creado
-sudo mysql -u root -e "flush privileges;" //reinicia todos los permisos para aplicar cambios
-sudo mysql -u root -e "create database web;" //crea DB para el login web
-
+sudo mysql -u root <<-EOF
+CREATE USER 'jaime'@'localhost' IDENTIFIED BY ''; #crea usuario en mysql
+GRANT ALL PRIVILEGES ON * . * TO 'jaime'@'localhost'; #da maximos privilegios al usuario creado
+flush privileges; #reinicia todos los permisos para aplicar cambios
+create database web; #crea DB para el login web
+use web;
+CREATE TABLE usuarios(nombre VARCHAR(30),apellidos VARCHAR(30),user VARCHAR(30) PRIMARY KEY UNIQUE,pas>
+INSERT INTO usuarios (nombre, apellidos, user, password) VALUES ('test', 'test', 'test@test.es', 'test>
+EOF
 
 sudo rm -r /var/www/html/*
 sudo mv /TFG_AWS/* /var/www/html
