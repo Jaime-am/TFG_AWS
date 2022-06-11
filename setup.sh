b26742a84f358bff294ddf7996ab5d66020e41b9
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-06-11 18:38:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ sudo mysql -u root <<-EOF
 CREATE USER 'jaime'@'localhost' IDENTIFIED BY ''; #crea usuario en mysql
 GRANT ALL PRIVILEGES ON * . * TO 'jaime'@'localhost'; #da maximos privilegios al usuario creado
 flush privileges; #reinicia todos los permisos para aplicar cambios
+EOF
+sudo mysql -u jaime <<-EOF
 create database web; #crea DB para el login web
 use web;
 CREATE TABLE usuarios (nombre VARCHAR(30),apellidos VARCHAR(30),user VARCHAR(30) PRIMARY KEY UNIQUE,password VARCHAR(100));
