70237325ef7a71c662df7d2147b5654be38f3c2c
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-06-18 14:55:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,14 +6,6 @@ sudo apt install php -y
 sudo apt install php-mysqli -y
 sudo mv 20-mysqli.ini /etc/php/8.1/apache2/conf.d/
 
-#Creamos un usuario www-data y lo añadimos al grupo www-data
-#para que pueda gestionar Apache la carpeta uploads
-sudo mkdir /var/www/html/uploads
-sudo adduser www-data www-data
-sudo chown -R www-data:www-data /var/www
-sudo chmod -R g+rwX /var/www
-sudo service apache2 restart
-
 sudo service mysql start
 
 sudo mysql -u root <<-EOF
@@ -28,6 +20,14 @@ use web;
 CREATE TABLE usuarios (nombre VARCHAR(30),apellidos VARCHAR(30),user VARCHAR(30) PRIMARY KEY UNIQUE,password VARCHAR(100));
 INSERT INTO usuarios (nombre, apellidos, user, password) VALUES ('test', 'test', 'test@test.es', 'test');
 EOF
+
+#Creamos un usuario www-data y lo añadimos al grupo www-data
+#para que pueda gestionar Apache la carpeta uploads
+sudo mkdir /var/www/html/uploads
+sudo adduser www-data www-data
+sudo chown -R www-data:www-data /var/www
+sudo chmod -R g+rwX /var/www
+sudo service apache2 restart
 
 sudo rm -r /var/www/html/*
 sudo mv /TFG_AWS/* /var/www/html
