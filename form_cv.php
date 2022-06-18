40da00a98c73df9f9784131c0e22a09b33118ded
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-06-18 13:29:40 +0200

    Update form_cv.php

--- form_cv.php
+++ form_cv.php
@@ -1,23 +1,23 @@
 <?php
 	if (isset($_POST['submit'])) {
-     		$target_path = "/var/www/html/uploads/";
-                $target_path = $target_path . basename( $_FILES['archivo']['name']);
-                if(!move_uploaded_file($_FILES['archivo']['tmp_name'], $target_path)) {
-                    
-                    echo '<pre>';
-                    echo 'Error en la subida de su PDF';
-                    echo '</pre>';
-                    
-                  } else {
+		$target_path = "/var/www/html/uploads/";
+		$target_path = $target_path . basename( $_FILES['archivo']['name']);
 
-                    $target_path = "/uploads/" . basename( $_FILES['archivo']['name']);
+		if(!move_uploaded_file($_FILES['archivo']['tmp_name'], $target_path)) {
+			
+			echo '<pre>';
+			echo 'Error en la subida de su PDF';
+			echo '</pre>';
+			
+		} else {
 
-echo <<<EOT
-<p>Su PDF ha sido subido con éxito.</p>
-<p>Puede consultarlo <a href=$target_path>aquí</a></p>
-EOT;
-                    
-                }
-     
-            }
+			$target_path = "/uploads/" . basename( $_FILES['archivo']['name']);
+
+				echo <<<EOT
+					<p>Su PDF ha sido subido con éxito.</p>
+					<p>Puede consultarlo <a href=$target_path>aquí</a></p>
+				EOT;
+			
+		}
+	}
 ?>
